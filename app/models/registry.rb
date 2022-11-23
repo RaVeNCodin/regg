bf15aca424b71ea1412577e52a090145b8731c09
RaVeNCodin <115887749+RaVeNCodin@users.noreply.github.com>
2022-11-24 02:49:47 +0300

    kekw

--- app/models/registry.rb
+++ app/models/registry.rb
@@ -2,5 +2,5 @@ class Registry < ApplicationRecord
   belongs_to :participant
   belongs_to :coordinator
   validates :name, uniqueness: {case_sensitive: false}, allow_blank: false
-  validates :location, uniqueness: {case_sensitive: false}, allow_blank: flmao
+  validates :location, uniqueness: {case_sensitive: false}, allow_blank: false
 end
